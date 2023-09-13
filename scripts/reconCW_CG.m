682d15ac8586d9f1a18ef3491d96717959f455f7
Thomas Ribeiro <ribeirothomas28@gmail.com>
2023-09-13 19:40:18 +0000

    Working CW and TD, cleanup

--- scripts/reconCW_CG.m
+++ scripts/reconCW_CG.m
@@ -1,41 +1,40 @@
 %% Diffuse Optical Tomography (DOT) reconstruction - CW mode with CG method
 % www.neuralimagery.com
 
-function reconCW_CG(depth, separation, resol, change, nopt)
-% clear all
-close all
+function reconCW_CG(depth, separation, square_width, change, nopt)
 
 %% 0. Define some parameters
-refind = 1.4;   % refractive index
-c0 = 0.3;       % speed of light in vacuum [mm/ps]
-cm = c0/refind; % speed of light in the medium [mm/ps]
-mua_bkg = 0.02; % background absorption [1/mm]
-mus_bkg = 0.67; % background scattering [1/mm];
-kap_bkg = 1./(3*(mua_bkg+mus_bkg)); % diffusion coefficient
 
-rad = 40;  % mesh radius [mm]
+% mesh parameters
+rad   = 70; % mesh radius [mm]
+nsect = 6;  % number of sectors
+nring = 32; % number of rings
+nbnd  = 2;  % number of boundary rings
 
-tau = 1e-3;   % regularisation parameter
-beta = 0.03;  % TV regularisation parameter
-itrmax = 500; % CG iteration limit
-tolCG = 1e-8; % convergence criterion
+% optical parameters @ 850nm
+refind = 1.4;                % refractive index
+c0     = 0.3;                % speed of light in vacuum [mm/ps]
+cm     = c0/refind;          % speed of light in the medium [mm/ps]
+mua0   = 0.02;               % background absorption [1/mm]
+mus0   = 0.67;               % background scattering [1/mm];
+
+bx = 64; by = 64; % solution basis: grid dimension
+
+% optimization parameters
+tau    = 1e-2; % regularisation parameter
+beta   = 0.01; % TV regularisation parameter
+tolCG  = 1e-8; % CG convergence criterion
+itrmax = 100;   % CG max. iterations
 
 %% 1. Define the target mesh
-% file = 'meshes/two_squares/two_squares_' + string(depth) + '_' + string(separation) + '_' + string(resol) + '.msh';
-% file = 'meshes/circle_two_squares/two_squares_' + string(depth) + '_' + string(separation) + '_' + string(resol) + '.msh';
-file = 'two_squares_' + string(depth) + '_' + string(separation) + '_' + string(resol) + '.msh';
-mesh = toastMesh(file,'gmsh');
-ne = mesh.ElementCount;
-nv = mesh.NodeCount;
-regidx = mesh.Region;
-regno = unique(regidx);
-abs = find((regidx == regno(2)) | (regidx == regno(3)));
-mesh.Display
+[vtx,idx,eltp] = mkcircle(rad,nsect,nring,nbnd);
+hMesh = toastMesh (vtx,idx,eltp);
+n = hMesh.NodeCount;
+hMesh.Display
 
 %% 2. Define the source and sensor geometry
 distrib = 'quarter';
 for i=1:nopt
-
   switch distrib
       case 'full'
           phiq = (i-1)/nopt*2*pi; 
@@ -44,23 +43,13 @@ for i=1:nopt
           phiq = (i-1)/nopt*pi/2+pi/4;
           phim = (i-0.5)/nopt*pi/2+pi/4;
   end
-
   source(i,:) = rad*[cos(phiq), sin(phiq)];
   sensor(i,:) = rad*[cos(phim), sin(phim)];
 end
 
-% mesh_height = 80;
-% mesh_width = 140;
-% shift = mesh_width/(2*nopt);
-% source(:,1) = linspace(0,mesh_width-shift,nopt);
-% source(:,2) = mesh_height;
-% 
-% sensor(:,1) = linspace(shift,mesh_width,nopt);
-% sensor(:,2) = mesh_height;
-
-mesh.SetQM(source,sensor);
-qvec = real(mesh.Qvec('Neumann','Gaussian',2));
-mvec = real(mesh.Mvec('Gaussian',2,refind));
+hMesh.SetQM(source,sensor);
+qvec = real(hMesh.Qvec('Neumann','Gaussian',2));
+mvec = real(hMesh.Mvec('Gaussian',2,refind));
 
 hold on
 plot(source(:,1),source(:,2),'ro','MarkerFaceColor','r');
@@ -69,54 +58,71 @@ hold off
 legend('','Source', 'Sensor');
 
 %% 3. Assign optical properties
-ref = ones(ne,1)*refind;
-mua = ones(ne,1)*mua_bkg; mua(abs) = mua_bkg*change;
-mus = ones(ne,1)*mus_bkg; mus(abs) = mus_bkg*change;
 
-figure;
-subplot(2,3,1); mesh.Display(mua);
-axis off; title('\mu_a target');
-subplot(2,3,2); mesh.Display(mus);
-axis off; title('\mu_s target');
+% Set up the mapper between FEM and inverse solution bases
+hBasis = toastBasis (hMesh, [bx by]);
+dx = 2*rad/bx; dy = 2*rad/by;
+
+% Set up homogeneous initial parameter estimates
+mua = ones(n,1) * mua0; mus = ones(n,1) * mus0; ref = ones(n,1) * refind;
+kap = 1./(3*(mua+mus));
+
+swidth    = round(square_width/dx);
+s1x_start = round(bx/2 - (separation/2)/dx - swidth/2);
+s2x_start = round(bx/2 + (separation/2)/dx - swidth/2);
+sy_start  = round(by-depth/dy-swidth); 
+
+muaim = zeros(bx,by); musim = muaim;
+muaim(s1x_start:s1x_start+swidth, sy_start:sy_start+swidth) = change*mua0;
+muaim(s2x_start:s2x_start+swidth, sy_start:sy_start+swidth) = change*mua0;
+musim(s1x_start:s1x_start+swidth, sy_start:sy_start+swidth) = change*mus0;
+musim(s2x_start:s2x_start+swidth, sy_start:sy_start+swidth) = change*mus0;
+
+mua1 = mua + hBasis.Map('B->M',muaim);
+mus1 = mus + hBasis.Map('B->M',musim);
+
+figure(2); clf;
+subplot(2,2,1); hMesh.Display(mua1); axis off; title('\mu_a target');
+subplot(2,2,2); hMesh.Display(mus1); axis off; title('\mu_s target');
 
 %% 4. Generate target data
-
-% compute perturbed target model
-data = log(mvec' * (dotSysmat(mesh, mua, mus, ref, 'EL')\qvec));
-
-% display sinogram of target data
-subplot(2,3,3); imagesc(data); axis equal tight; colorbar
-title('target data');
+data = log(mvec' * (dotSysmat(hMesh, mua1, mus1, ref, 0)\qvec));
+data = reshape(data, [], 1);
 
 %% 5. Setup inverse solver
 
 % reset parameters to background values
-ref = ones(nv,1)*refind;
-mua = ones(nv,1)*mua_bkg; mus = ones(nv,1)*mus_bkg; kap = ones(nv,1)*kap_bkg;
-Y = log(mvec' * (dotSysmat(mesh, mua, mus, ref)\qvec));
-    
-proj = reshape(Y, [], 1); data = reshape(data, [], 1); sd = ones(size(proj));
-subplot(2,3,4); mesh.Display(mua); axis off; title('\mu_a recon');
-subplot(2,3,5); mesh.Display(mus); axis off; title('\mu_s recon');
-subplot(2,3,6); imagesc(reshape(proj,nopt,nopt)); axis equal tight; colorbar; title('recon data');
+proj = log(mvec' * (dotSysmat(hMesh, mua, mus, ref, 0)\qvec));
+proj = reshape(proj, [], 1);
+sd = proj; % Assume standard deviation equal to data
 
-% setup inverse solver basis on grid
-grd = [32,32]; basis = toastBasis(mesh, grd);
+% display sinogram of target data
+figure(3); clf;
+subplot(1,2,1); imagesc(reshape(data-proj,nopt,nopt)); axis equal tight; colorbar
+title('initial error');
+
+figure(2);
+subplot(2,2,3); hMesh.Display(mua); axis off; title('\mu_a recon');
+subplot(2,2,4); hMesh.Display(mus); axis off; title('\mu_s recon');
 
 % map initial estimate of the solution vector
-bmua = basis.Map ('M->B', mua); bcmua = bmua*cm; scmua = basis.Map ('B->S', bcmua); p = length(scmua);
-bkap = basis.Map ('M->B', kap); bckap = bkap*cm; sckap = basis.Map ('B->S', bckap);
+bmua = hBasis.Map ('M->B', mua); bcmua = bmua*cm; scmua = hBasis.Map ('B->S', bcmua);
+bkap = hBasis.Map ('M->B', kap); bckap = bkap*cm; sckap = hBasis.Map ('B->S', bckap);
 
 % solution vector
-x = [scmua;sckap]; logx = log(x);
+x = [scmua;sckap]; 
+% logx = log(x);
 
 % setup regularizer instance
-regul = toastRegul('TV', basis, logx, tau, 'Beta', beta);
+% hReg = toastRegul('TK1', hBasis, x, 1);
+% hReg = toastRegul('TK1',hBasis,logx,1);
+hReg = toastRegul ('TV', hBasis, x, tau, 'Beta', beta);
+% hReg = toastRegul ('Huber', hBasis, logx, tau, 'Eps', beta);
 
 % compute initial value of the objective function
-err0 = toastObjective(proj, data, sd, regul, logx);
-err = err0; errp = inf;
-itr = 1; step = 1.0;
+err0 = toastObjective(proj, data, sd, hReg, x);
+err = err0; errp = 1e10;
+itr = 1; step = 0.02;
 fprintf('Iteration %d, objective %f\n', 0, err);
 
 %% 6. Run the inverse loop solver
@@ -124,10 +130,10 @@ fprintf('Iteration %d, objective %f\n', 0, err);
 while (itr <= itrmax) && (err > tolCG*err0) && (errp-err > tolCG)
 
     errp = err;
-    r = -toastGradient(mesh, basis, qvec, mvec, mua, mus, ref, 0, ...
+    r = -toastGradient(hMesh, hBasis, qvec, mvec, mua, mus, ref, 0, ...
         data, sd, 'method', 'cg', 'tolerance', 1e-12);
     % r = r(1:p); % drop mus gradient
-    r = r .* x; r = r - regul.Gradient(logx);
+    r = r .* x; r = r - hReg.Gradient(x);
 
     if itr > 1
         delta_old = delta_new;
@@ -146,34 +152,35 @@ while (itr <= itrmax) && (err > tolCG*err0) && (errp-err > tolCG)
             d = s + d*beta;
         end
     end
-    step = toastLineSearch(logx, d, step, err, @objective);
-    logx = logx + d*step;
+    step = toastLineSearch(x, d, step, err, @objective);
+    x = x + d*step;
     
-    smua = exp(logx(1:p))/cm; mua = basis.Map ('S->M', smua);
-    skap = exp(logx(p+1:end))/cm; 
-    smus = 1./(3*skap) - smua; mus = basis.Map ('S->M', smus);
-    subplot(2,3,4); mesh.Display(real(mua)); axis off; title('\mu_a recon');
-    subplot(2,3,5); mesh.Display(real(mus)); axis off; title('\mu_s recon');
+    smua = x(1:size(x)/2)/cm; mua = hBasis.Map ('S->M', smua);
+    skap = x(size(x)/2+1:size(x))/cm; 
+    smus = 1./(3*skap) - smua; mus = hBasis.Map ('S->M', smus);
+    
+    figure(2);
+    subplot(2,2,3); hMesh.Display(mua); axis off; title('\mu_a recon');
+    subplot(2,2,4); hMesh.Display(mus); axis off; title('\mu_s recon');
 
-    proj = reshape(log(mvec' * (dotSysmat(mesh, mua, mus, ref)\qvec)), [], 1);
-    subplot(2,3,6); imagesc(reshape(proj,nopt,nopt)); axis equal tight; colorbar; title('recon data');
-    err = toastObjective(proj, data, sd, regul, logx);
-    % fvals(itr) = err;
+    proj = reshape(log(mvec' * (dotSysmat(hMesh, mua, mus, ref, 0)\qvec)), [], 1);
+    err = toastObjective(proj, data, sd, hReg, x);
     fprintf('Iteration %d, objective %f\n', itr, err);
     itr = itr+1;
     drawnow
 end
 
-    function o = objective(logx)
-        smua_ = exp(logx(1:p))/cm; mua_ = basis.Map ('S->M', smua_);
-        skap_ = exp(logx(p+1:end))/cm; 
-        smus_ = 1./(3*skap_) - smua_; mus_ = basis.Map ('S->M', smus_);
-        proj_ = reshape(log(mvec' * (dotSysmat(mesh, mua_, mus_, ref)\qvec)), [], 1);
-        o = toastObjective(proj_, data, sd, regul, logx);
+    function o = objective(x)
+        smua_ = x(1:size(x)/2)/cm; mua_ = hBasis.Map ('S->M', smua_);
+        skap_ = x(size(x)/2+1:size(x))/cm; 
+        smus_ = 1./(3*skap_) - smua_; mus_ = hBasis.Map ('S->M', smus_);
+        proj_ = reshape(log(mvec' * (dotSysmat(hMesh, mua_, mus_, ref, 0)\qvec)), [], 1);
+        o = toastObjective(proj_, data, sd, hReg, x);
     end
 
-path = 'results/circle_two_squares/';
-filename = 'recon_'+string(depth)+'_'+string(separation)+'_'+string(resol)+'_'+string(change)+'_'+string(nopt);
+figure(3); subplot(1,2,2); imagesc(reshape(data-proj,nopt,nopt)); axis equal tight; colorbar; title('final error');
+path = 'results/circle_two_squares/CG/';
+filename = 'recon_'+string(depth)+'_'+string(separation)+'_'+string(square_width)+'_'+string(change)+'_'+string(nopt);
 save(path + filename + '.mat');
 saveas(gcf, path + filename + '.fig');
 % figure; plot(fvals); xlabel('Iteration No.'); ylabel('Objective function values');
--- scripts/reconTD_GN.m
+++ scripts/reconTD_GN.m
@@ -9,18 +9,12 @@
 % User-defined parameters
 % ======================================================================
 
-function reconTD_GN(depth, separation, resol, change, nopt)
+function reconTD_GN(depth, separation, square_width, change, nopt)
 
 %% 0. Define parameters
 
-% remove parameters
-nopt       = 8;   % number of sources (and detectors)
-depth      = 10;  % [mm]
-separation = 20;  % [mm]
-change     = 2;   % percent change in optical properties
-
 % mesh parameters
-rad   = 40; % mesh radius [mm]
+rad   = 70; % mesh radius [mm]
 nsect = 6;  % number of sectors
 nring = 32; % number of rings
 nbnd  = 2;  % number of boundary rings
@@ -31,24 +25,21 @@ c0     = 0.3;                % speed of light in vacuum [mm/ps]
 cm     = c0/refind;          % speed of light in the medium [mm/ps]
 mua0   = 0.02;               % background absorption [1/mm]
 mus0   = 0.67;               % background scattering [1/mm];
-kap0   = 1./(3*(mua0+mus0)); % diffusion coefficient
 
 % temporal parameters
 dt    = 20;  % time step in picoseconds
 nstep = 256; % number of time steps
 ww    = 32;  % -> 7 time bins of width 32 (in units of time-step)
 twin  = zeros(nstep/ww-1,2); twin(1:nstep/ww-1,1) = [1:ww:(nstep-ww)]';
-twin(:,2) = twin(:,1)+ww-1;  
+twin(:,2) = twin(:,1)+ww-1;
 nwin  = size(twin,1);
 
-% solution basis: grid dimension
-bx = 64; by = 64;                    
-blen = bx*by;
+bx = 512; by = 512; % solution basis: grid dimension
 
 % optimization parameters
-tau    = 1e-2;    % regularisation parameter
+tau    = 1e-2; % regularisation parameter
 beta   = 0.01; % TV regularisation parameter
-GNtol  = 5e-4; % Gauss-Newton convergence criterion
+GNtol  = 5e-6; % Gauss-Newton convergence criterion
 itrmax = 20;   % Gauss-Newton max. iterations
 tol    = 1e-4;
 maxit  = 100;
@@ -57,7 +48,6 @@ maxit  = 100;
 [vtx,idx,eltp] = mkcircle(rad,nsect,nring,nbnd);
 hMesh = toastMesh (vtx,idx,eltp);
 n = hMesh.NodeCount;
-dmask = hMesh.DataLinkList ();
 bb = hMesh.BoundingBox();
 bmin = bb(1,:); bmax = bb(2,:);
 vscale = (bmax(1)-bmin(1))/bx * (bmax(2)-bmin(2))/by * cm;
@@ -92,19 +82,16 @@ legend('','Source', 'Sensor');
 
 % Set up the mapper between FEM and inverse solution bases
 hBasis = toastBasis (hMesh, [bx by]);
-dx = 2*rad/by; dy = 2*rad/by;
-% solmask = hBasis.Map('S->B',ones(hBasis.slen,1));
-% figure; imagesc(reshape(solmask,bx,by)); title('solution mask');
+dx = 2*rad/bx; dy = 2*rad/by;
 
 % Set up homogeneous initial parameter estimates
 mua = ones(n,1) * mua0; mus = ones(n,1) * mus0; ref = ones(n,1) * refind;
 kap = 1./(3*(mua+mus));
 
-% Setup up heterogenous parameter estimates
-swidth    = int32(8/dx);
-s1x_start = int32(1+bx/2 - (separation/2)/dx - swidth/2);
-s2x_start = int32(bx/2 + (separation/2)/dx - swidth/2);
-sy_start  = int32(by-depth/dy-swidth);
+swidth    = round(square_width/dx);
+s1x_start = round(bx/2 - (separation/2)/dx - swidth/2);
+s2x_start = round(bx/2 + (separation/2)/dx - swidth/2);
+sy_start  = round(by-depth/dy-swidth); 
 
 muaim = zeros(bx,by); musim = muaim;
 muaim(s1x_start:s1x_start+swidth, sy_start:sy_start+swidth) = change*mua0;
@@ -119,10 +106,6 @@ figure(2); clf;
 subplot(2,2,1); hMesh.Display(mua1); axis off; title('\mu_a target');
 subplot(2,2,2); hMesh.Display(mus1); axis off; title('\mu_s target');
 
-% figure(4);clf;
-% subplot(1,2,1); imagesc(reshape(hBasis.Map('M->B',mua0*ones(n,1)),bx,by) + muaim);title('\mu_a target');
-% subplot(1,2,2); imagesc(reshape(hBasis.Map('M->B',mus0*ones(n,1)),bx,by) + musim);title('\mu_s target');
-
 %% 4. Generate target data
 
 [data,t] = toastProjectTPSF(hMesh, mua1, mus1, ref, qvec, mvec, dt, nstep);
@@ -136,28 +119,30 @@ m = length(data);
 proj = reshape(WindowTPSF(proj,twin)',[],1); % single vector of data, ordered by time gate.
 sd = proj; % Assume standard deviation equal to data
 
-figure(3); clf; sgtitle('(Target - Initial) Data')
+figure(3); clf; sgtitle('initial error')
 for w = 1:min(nwin,8)
     dl = reshape(data((w-1)*nM*nQ+1:w*nM*nQ)-proj((w-1)*nM*nQ+1:(w)*nM*nQ),nM,nQ);
     subplot(2,4,w); imagesc(dl);
 end
 
+figure(2);
+subplot(2,2,3); hMesh.Display(mua); axis off; title('\mu_a target');
+subplot(2,2,4); hMesh.Display(mus); axis off; title('\mu_s target');
+
 % map initial estimate of the solution vector
-% bmus = hBasis.Map('M->B', mus);
 bmua = hBasis.Map ('M->B', mua); bcmua = bmua*cm; scmua = hBasis.Map ('B->S', bcmua);
 bkap = hBasis.Map ('M->B', kap); bckap = bkap*cm; sckap = hBasis.Map ('B->S', bckap);
-% scmua = bcmua(find(solmask==1));
-% sckap = bckap(find(solmask==1));
 
 % solution vector
 x = [scmua; sckap]; % logx = log(x);
 p = length(x); 
 
 % setup regularizer instance
-hReg = toastRegul('TK1', hBasis, x, 1);
-%hReg = toastRegul('TK1',hBasis,logx,1);
-%hReg = toastRegul ('TV', hBasis, logx, tau, 'Beta', beta);
-%hReg = toastRegul ('Huber', hBasis, logx, tau, 'Eps', beta);
+% hReg = toastRegul('TK1', hBasis, x, 1);
+% hReg = toastRegul('TK1',hBasis,logx,1);
+hReg = toastRegul ('TV', hBasis, x, tau, 'Beta', beta);
+% hReg = toastRegul ('TV', hBasis, logx, tau, 'Beta', beta);
+% hReg = toastRegul ('Huber', hBasis, logx, tau, 'Eps', beta);
 
 % compute initial value of the objective function
 err0 = toastObjective(proj, data, sd, hReg, x);
@@ -192,7 +177,8 @@ while (itr <= itrmax) & (err > GNtol*err0) & (errp-err > GNtol)
     % end
 
     M = ones(1,size(J,2));               % override preconditioner
-    hReg = toastRegul('TK1',hBasis,x,1); % no hyperparameter set here
+    % hReg = toastRegul('TK1',hBasis,x,1); % no hyperparameter set here
+    % hReg = toastRegul ('TV', hBasis, x, tau, 'Beta', beta);
     Hr = hReg.Hess(x);
     y = (data-proj)./sd;
     r = J' * y;                          % gradient of cost function
@@ -243,5 +229,16 @@ end
         [p, p_data, p_prior] = toastObjective (proj_, data, sd, hReg, x);
         fprintf (1, '    [LH: %f, PR: %f]\n', p_data, p_prior);
     end
+
+figure(4); clf; sgtitle('final error')
+for w = 1:min(nwin,8)
+    dl = reshape(data((w-1)*nM*nQ+1:w*nM*nQ)-proj((w-1)*nM*nQ+1:(w)*nM*nQ),nM,nQ);
+    subplot(2,4,w); imagesc(dl);
+end
+path = 'results/circle_two_squares/TD/';
+filename = 'recon_'+string(depth)+'_'+string(separation)+'_'+string(square_width)+'_'+string(change)+'_'+string(nopt);
+save(path + filename + '.mat');
+saveas(gcf, path + filename + '.fig');
+% figure; plot(fvals); xlabel('Iteration No.'); ylabel('Objective function values');
 % save -v7.3 Jrecon J data y;
 end
