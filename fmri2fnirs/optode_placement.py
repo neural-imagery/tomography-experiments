59cba84e543fbd29695c15ce2d27fb8e3db8c0ee
Thomas Ribeiro <ribeirothomas28@gmail.com>
2023-09-16 00:38:32 +0000

    segmentation pipeline complete

--- fmri2fnirs/optode_placement.py
+++ fmri2fnirs/optode_placement.py
@@ -8,10 +8,50 @@ import jdata as jd
 from scipy import ndimage
 from skimage import measure
 from scipy.spatial import KDTree
-from scipy.linalg import svd 
+from scipy.linalg import svd
 
-def get_optodes(vol:np.ndarray):
+def get_probes(n:int, vertices):
+    probes = vertices[np.random.choice(np.arange(len(vertices)), 4*n)]     # randomly over sample vertices
+    print(f"{len(probes)} probes left")
+    vertices_z = vertices[:,2]
+    vertices_mid = (min(vertices_z) + max(vertices_z))/2                   # mid-point, only put electrodes on top half of brain
+    probes_z = probes[:,2]
+    probes = probes[np.where(probes_z >= vertices_mid)]                    # throw away bottom half
+    print(f"{len(probes)} probes left")
+    probes = probes[:n]
+    print(f"{len(probes)} probes left")
+    c = np.mean(vertices,axis=0)                                           # Coordinate of center of mass
+    probes = np.array([i*0.99+c*0.01 for i in probes])                     # bring sources a tiny bit into the brain 1%, make sure in tissue
+    return probes
+
+def get_normals(sources, vertices):
+    c = np.mean(vertices, axis=0) # center point
+    normals = []
+    kdtree = KDTree(vertices)
+    for em in sources:
+        _, neighbours_indices = kdtree.query(em, k=10)
+        neighbours_10 = vertices[neighbours_indices]
+        mean_point = np.mean(neighbours_10, axis=0)
+        centered_points = neighbours_10 - mean_point
+        _, _, Vt = svd(centered_points)
+        normal_vector = Vt[-1, :]
+        normal_vector /= np.linalg.norm(normal_vector)
+        # we want our normals to point inwards
+        if normal_vector @ (c-em) < 0:
+            normal_vector *= -1
+        normals.append(normal_vector)
+    return np.asarray(normals)
+
+def padz_with0layer(vol: np.ndarray):
     """
+    Add a layer of zeros at the top so that the surface finds the top of the brain
+    """
+    return np.vstack([vol.T, np.zeros([1,120,120])]).T
+
+def get_optodes(vol:np.ndarray, nsources: int = 10, ndetectors: int = 100):
+    """
+    Get optode locations from brain segmentation
+
     Parameters
     ----------
     vol : np.ndarray
@@ -26,52 +66,24 @@ def get_optodes(vol:np.ndarray):
     directions : list of np.ndarray
         List of unit vectors (x,y,z), pointint normal to brian towards center of brain
     """
-    def padz_with0layer(arr3):
-        return np.vstack([arr3.T, np.zeros([1,120,120])]).T
+    
     vol = padz_with0layer(vol)                                                 # pad z layer with layer of zeros for better edges
+    
     # make mesh
     vertices, faces,_,_ = measure.marching_cubes(vol, level=0.5)
     vertices = np.asarray(vertices,'float')
-    zthresh = (max(vertices[:,2]) + min(vertices[:,2]))/2
-    zmask = vertices[:,2] > zthresh
-    vert_half = vertices[zmask]
-    face_indices_to_keep = np.isin(faces, np.where(zmask)).all(axis=1)
-    filtered_faces = faces[face_indices_to_keep]
+
+    # zthresh = (max(vertices[:,2]) + min(vertices[:,2]))/2
+    # zmask = vertices[:,2] > zthresh
+    # vert_half = vertices[zmask]
+    # face_indices_to_keep = np.isin(faces, np.where(zmask)).all(axis=1)
+    # filtered_faces = faces[face_indices_to_keep]
+    
+    
     # place optodes randomly
-    N=50 # number of optodes to place
-    def get_probes(n=N,vertices=vertices):
-        probes = vertices[np.random.choice(np.arange(len(vertices)), 4*n)]     # randomly over sample vertices
-        print(f"{len(probes)} probes left")
-        vertices_z = vertices[:,2]
-        vertices_mid = (min(vertices_z) + max(vertices_z))/2                   # mid-point, only put electrodes on top half of brain
-        probes_z = probes[:,2]
-        probes = probes[np.where(probes_z >= vertices_mid)]                    # throw away bottom half
-        print(f"{len(probes)} probes left")
-        probes = probes[:n]
-        print(f"{len(probes)} probes left")
-        c = np.mean(vertices,axis=0)                                           # Coordinate of center of mass
-        probes = np.array([i*0.99+c*0.01 for i in probes])                     # bring sources a tiny bit into the brain 1%, make sure in tissue
-        return probes
-    def get_normals(emmitors, vertices):
-        c = np.mean(vertices, axis=0) # center point
-        normals = []
-        kdtree = KDTree(vertices)
-        for em in emmitors:
-            _, neighbours_indices = kdtree.query(em, k=10)
-            neighbours_10 = vertices[neighbours_indices]
-            mean_point = np.mean(neighbours_10, axis=0)
-            centered_points = neighbours_10 - mean_point
-            _, _, Vt = svd(centered_points)
-            normal_vector = Vt[-1, :]
-            normal_vector /= np.linalg.norm(normal_vector)
-            # we want our normals to point inwards
-            if normal_vector @ (c-em) < 0:
-                normal_vector *= -1
-            normals.append(normal_vector)
-        return np.asarray(normals)
-    detectors = get_probes(80)                                                 # Initiate 80 detectors
-    sources = get_probes(10)                                                   # Initiate 10 probes
-    directions = get_normals(sources,vertices)                                 # Find orthogonal directions of sources (pointing into brain)
+    detectors = get_probes(ndetectors, vertices)                                                 # Initiate 100 detectors
+    sources = get_probes(nsources, vertices)                                                   # Initiate 10 probes
+    directions = get_normals(sources, vertices)                                 # Find orthogonal directions of sources (pointing into brain)
     return sources,detectors,directions 
 
 def arr3_to_mcx_input(numpy_fname:str, json_boilerplate:str="colin27.json"):
