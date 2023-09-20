fb00f3639154ac703732360d4ffcb060886dcf1f
Thomas Ribeiro <ribeirothomas28@gmail.com>
2023-09-20 08:17:34 +0000

    5x speedup

--- fmri2fnirs/subject.py
+++ fmri2fnirs/subject.py
@@ -10,6 +10,7 @@ import jdata as jd
 import plotly.graph_objects as go
 from scipy.ndimage import affine_transform
 import pmcx
+import time
 
 
 class Subject(object):
@@ -87,14 +88,27 @@ class Subject(object):
     def place_optodes(self, nsources: int = 10, ndetectors: int = 100, 
                         detrad : float = 3, frac_closer=0.03, display_setup=False):
         """
-        Get optode locations from brain segmentation
+        Place optodes uniformly on head given the subject's brain segmentation.
+
+        Parameters
+        ----------
+        nsources : int
+            Number of sources
+        ndetectors : int
+            Number of detectors
+        detrad : float
+            Detector radius (in mm)
+        frac_closer : float
+            Percent for which to bring optodes closer to the center
+        display_setup : bool
+            Whether to display the optode setup
         """
         
         self.geom_path = self.anat_path + f"fnirs_geometry_{nsources}_{ndetectors}_{detrad}_{frac_closer}/"
         if os.path.exists(self.geom_path): 
             sources = np.load(self.geom_path+'source_positions.npy')
             directions = np.load(self.geom_path+'source_directions.npy')
-            detectors = np.load(self.geom_path+'detector_positions.npy')
+            detectors = np.load(self.geom_path+'parent_detector_positions.npy')
             self.geometry = Geometry(sources, detectors, directions)
 
         else:
@@ -124,46 +138,84 @@ class Subject(object):
             # Find orthogonal directions of sources (pointing into brain)
             directions = utils.get_normals(sources, vertices)
             
-            # create class attributes
+            # create geometry
             self.geometry = Geometry(sources, detectors, directions)
 
             # save source positions, source directions and detector positions 
             np.save(self.geom_path+'source_positions.npy', self.geometry.sources)
             np.save(self.geom_path+'source_directions.npy', self.geometry.directions)
-            np.save(self.geom_path+'detector_positions.npy', self.geometry.detectors)
+            np.save(self.geom_path+'parent_detector_positions.npy', self.geometry.detectors)
 
-        if display_setup:
-            utils.display_3d(self.segmentation, self.geometry)
+            # initialize source and closest detectors subset
+            self.geometry.initialize_src_dets(self.geom_path)
+
+        if display_setup: utils.display_3d(self.segmentation, self.geometry)
+    
+    
+    ###########################################################################
+    # MCX
+    ###########################################################################
+    
+    def compute_jacobian(self, props):
+        """
+        Compute the absorption (μ_a) Jacobian (nsrc, nx, ndet) for the subject's 
+        optode geometry.
+
+        Parameters
+        ----------
+        props : list of tuples
+            Optical properties (μ_a, μ_s, g, n) for each tissue type in the 
+            segmentation. 
+        """
+
+        # define paths and create directories
+        data_path = self.geom_path + "baseline/" 
+        jac_path  = self.geom_path + "jacobians/"
+        if not os.path.exists(data_path): os.makedirs(data_path)
+        if not os.path.exists(jac_path):  os.makedirs(jac_path)
+
+        for src_idx in range(self.geometry.nsources):
+            if not os.path.isfile(jac_path+f"jac_src{src_idx}.npy"):
+
+                # run forward model and save detector data
+                res, cfg = utils.forward(self.segmentation, self.geometry, props, src_idx=src_idx)
+                data = utils.get_detector_data(res, cfg)
+                np.save(data_path+f"base_src{src_idx}.npy", data)
+            
+                # define cfg['seed'] using the returned seeds
+                cfg['seed'] = res['seeds']
+
+                # define cfg['detphotons'] using the returned detp data
+                cfg['detphotons'] = res['detp']
+
+                # ensure output is absorption (μ_a) Jacobian
+                cfg['outputtype'] = 'jacobian'
+
+                res2 = pmcx.run(cfg)
+                np.save(self.geom_path+f"jacobians/jac_src{src_idx}.npy", np.squeeze(res2['flux']))
     
     
     ###########################################################################
     # fMRI
     ###########################################################################
-    
-    def compute_jacobian(self, props, display_jacobian=False, time_idx=None):
+
+    def transform(self, sessionID, runID, jax=True, display_setup=False):
         """
-        Compute the Jacobian matrix for the subject's optode geometry.
-        """
-        
-        for src_idx in range(1):#self.geometry.nsources):
+        Transform the segmentation and optodes into the run's functional space.
 
-            res, cfg = utils.forward(self.segmentation, self.geometry, props, src_idx=src_idx)
-        
-            cfg['seed']       = res['seeds']  # one must define cfg['seed'] using the returned seeds
-            cfg['detphotons'] = res['detp']   # one must define cfg['detphotons'] using the returned detp data
-            cfg['outputtype'] = 'jacobian'    # tell mcx to output absorption (μ_a) Jacobian
+        Parameters
+        ----------
+        sessionID : int
+        runID : int
+        display_setup : bool
+            Whether to display the transformed segmentation and optode geometry.
 
-            res2 = pmcx.run(cfg)
-            np.save(self.geom_path+f"jac_src{src_idx}.npy", np.squeeze(res2['flux']))
-
-        if display_jacobian:
-            if time_idx is None: time_idx = 0
-            utils.display_3d(res2['flux'][:,:,:,time_idx], self.geometry)
-
-
-    def transform(self, sessionID, runID, display_setup=False):
-        """
-        Bring the segmentation and optodes into the run's functional space.
+        Returns
+        -------
+        seg_transformed : np.ndarray
+            Transformed segmentation
+        geometry : Geometry
+            Transformed optode geometry
         """
 
         resolution = "1pt0"
@@ -184,28 +236,26 @@ class Subject(object):
             url = f"https://natural-scenes-dataset.s3.amazonaws.com/nsddata_rawdata/sub-{self.id}/ses-nsd{sessionID}/func/"
             utils.download_nsd_file(url, func_path, fmri_file)
 
-            # register anatomical segmention in functional space using FSLs `flirt`
-            # anat_seg_file = f"data/sub{self.id}/anat/T1_{resolution}_masked_head_seg.nii.gz"
-            # os.system(f"flirt -in {anat_seg_file} -ref {func_path}{fmri_file}{ext} -out {func_path}{seg_file}{ext}")
-
-            # get transform matrix
+            # get anatomical-to-functional transform matrix
             os.system(f"flirt -in {self.anat_path}{T1_file}{ext} -ref {func_path}{fmri_file}{ext} -omat {func_path}anat2func.mat")
+            anat2func = np.loadtxt(func_path+"anat2func.mat")
 
             # apply transform to segmentation
-            # os.system(f"flirt -in {self.anat_path}{T1_file}_{seg_file}{ext} -ref {func_path}{fmri_file}{ext} -applyxfm -init {func_path}anat2func.mat -out {func_path}{seg_file}{ext}")
-            # seg = np.round(nib.load(func_path+seg_file+ext).get_fdata()).astype('uint8')
-            seg = utils.transform_volume(self.segmentation, np.loadtxt(func_path+"anat2func.mat"), output_shape=nib.load(func_path+fmri_file+ext).get_fdata().shape[:-1])
+            output_shape = (120,120,84) # HARD-CODED to save time
+            # output_shape = nib.load(func_path+fmri_file+ext).get_fdata().shape[:-1]
+            if jax: seg = utils.transform_volume_jax(self.segmentation, anat2func, output_shape)
+            else: seg = utils.transform_volume(self.segmentation, anat2func, output_shape)
             seg = np.round(seg).astype('uint8')
             np.save(func_path+seg_file+".npy", seg)
         
-        # transform geometry (with scaling 1/1.8 scaling)
+        # transform optode geometry
+        scaling = 1.8
         anat2func = np.loadtxt(func_path+"anat2func.mat")
         sources   = utils.transform_points(self.geometry.sources, anat2func)
         detectors = utils.transform_points(self.geometry.detectors[:,:3], anat2func)
-        detectors = np.hstack([detectors, 2*self.geometry.detectors[:,3:]])
+        detectors = np.hstack([detectors, scaling*self.geometry.detectors[:,3:]])
         directions = (anat2func[:3,:3]@self.geometry.directions.T).T
         directions = directions / np.linalg.norm(directions, axis=1, keepdims=True)
-        
         geometry = Geometry(sources, detectors, directions)
         # utils.save_optodes_json(seg, geometry)
 
@@ -214,50 +264,72 @@ class Subject(object):
         return seg, geometry
     
 
-    def get_optics(self, sessionID, runID, seg, props):
+    def get_optics(self, sessionID, runID, seg):
         """
-        Get optical baseline and time-varying changes in optical baseline 
-        (only mu_a for now) over the fMRI run.
+        Get time-varying μ_a changes from optical baseline over the fMRI run.
+
+        Parameters
+        ----------
+        sessionID : int
+        runID : int
+        seg : np.ndarray
+            Segmentation
+        props : list of tuples
+            Optical properties (μ_a, μ_s, g, n) for each tissue type
+
+        Returns
+        -------
+        dmua_850 : np.ndarray
+            Time-varying μ_a changes at 850nm
+        dmua_690 : np.ndarray
+            Time-varying μ_a changes at 690nm
         """
-        
-        # baseline
-        newshape = (2, *seg.shape)
-        optical_baseline = np.zeros(newshape)
-        for idx, prop in enumerate(props):
-            # when seg_transformed == idx, optical_baseline[0] == prop[0], optical_baseline[1] == prop[1]
-            optical_baseline[0][seg == idx] = prop[0] # mu_a
-            optical_baseline[1][seg == idx] = prop[1] # mu_s
 
         # changes in baseline over fMRI run
         fmri = nib.load(f'data/sub{self.id}/func/sess{sessionID}/run{runID}/sub-{self.id}_ses-nsd{sessionID}_task-nsdcore_run-{runID}_bold.nii.gz').get_fdata()
         fmri_inv = 1 / (fmri + 1e-9)
         fmri_inv_avg = np.average(fmri_inv, axis=3)
         fmri_inv_percent = (fmri_inv - fmri_inv_avg[:,:,:,np.newaxis]) / fmri_inv_avg[:,:,:,np.newaxis]
+        
         # clip to +/- 40%
         fmri_inv_percent = np.clip(fmri_inv_percent, -0.4, 0.4)
         # plot_bold(seg_transformed, bold_percent[...,0])
         dmua_690, dmua_850 = utils._boldpercent2optical(fmri_inv_percent, seg)
         
-        return optical_baseline, dmua_850, dmua_690
+        return dmua_850, dmua_690
     
 
-    def get_jacobian(self, sessionID, runID, src_idx):
+    def get_jacobian(self, sessionID, runID, src_idx, jax=True):
         """
-        Get jacobian in functional space for a given source.
+        Get μ_a Jacobian for a single source and transform into the run's functional 
+        space.
+
+        Parameters
+        ----------
+        sessionID : int
+        runID : int
+        src_idx : int
+
+        Returns
+        -------
+        J_transformed : np.ndarray
         """
 
-        func_path = f"data/sub{self.id}/func/sess{sessionID}/run{runID}/"
-        jac_file = f"jac_src{src_idx}"
-        fmri_file = f"sub-{self.id}_ses-nsd{sessionID}_task-nsdcore_run-{runID}_bold"
-        ext  = '.nii.gz'
-
-        # get Jacobian computed in anatomical space
-        J = np.load(self.geom_path+jac_file+'.npy')
+        # get Jacobian in anatomical space
+        J = np.load(self.geom_path+f"jacobians/jac_src{src_idx}"+'.npy')
         
         # transform Jacobian into functional space
-        output_shape = nib.load(func_path+fmri_file+ext).get_fdata().shape[:-1]
-        J_transformed = utils.transform_volume(J, np.loadtxt(func_path+"anat2func.mat"), output_shape=output_shape)
-        J_transformed = np.reshape(J_transformed, (np.prod(output_shape), self.geometry.ndetectors))
+        func_path = f"data/sub{self.id}/func/sess{sessionID}/run{runID}/"
+        anat2func = np.loadtxt(func_path+"anat2func.mat")
+        output_shape = (120,120,84) # HARD-CODED to save time
+        #output_shape = np.load(func_path+'head_seg.npy').shape
+        ndetectors   = np.load(self.geom_path+f"detector_positions/detectors_src{src_idx}.npy").shape[0]
+        
+        if jax: 
+            J_transformed = utils.transform_volume_jax(J, anat2func, output_shape=output_shape)
+        else: 
+            J_transformed = utils.transform_volume(J, anat2func, output_shape=output_shape)
+        J_transformed = np.reshape(J_transformed, (np.prod(output_shape), ndetectors))
 
         return J_transformed
 
