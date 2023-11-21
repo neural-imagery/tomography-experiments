22fa9fc25de605b281dacfe945c9db7028acef3c
Raffi Hotter <raphael.hotter@gmail.com>
2023-11-21 04:52:01 +0000

    Refactor

---
+++ 3d/problems/two_balls.py
@@ -0,0 +1,126 @@
+import numpy as np
+from medium import Medium
+from sensor_geometry import SensorGeometry
+
+# head constants
+r_head_mm = 70
+
+
+def two_balls_2d_medium(
+    contrast=1.1, voxels_per_dim=150, r_ball_mm=5, depth_mm=20, ball_separation_mm=10
+):
+    """
+    Builds a 2D medium with two balls of different optical properties.
+
+    Parameters
+    ----------
+    contrast : float
+        The relative change in absorption of the perturbation.
+    voxels_per_dim : int, optional
+        The number of voxels along each dimension of the medium. Default is 150.
+    r_ball_mm : float, optional
+        The radius of the ball in millimeters. Default is 5 mm.
+    depth_mm : float, optional
+        The depth of the balls in millimeters. Default is 20 mm.
+    ball_separation_mm : float, optional
+        The separation between the balls in millimeters. Default is 10 mm.
+
+    Returns
+    -------
+    medium : Medium
+        The 2D medium with two balls of different optical properties.
+    """
+    nz = 1
+    ny = voxels_per_dim
+    nx = voxels_per_dim
+    medium = Medium((nz, ny, nx))
+
+    # head
+    medium.add_ball((nz // 2, ny // 2, nx // 2), r_head_mm, 1)
+
+    # add balls
+    medium.add_ball(
+        (nz // 2, depth_mm, nx // 2 - np.floor(ball_separation_mm)), r_ball_mm, 2
+    )
+    medium.add_ball(
+        (nz // 2, depth_mm, nx // 2 + np.ceil(ball_separation_mm)), r_ball_mm, 2
+    )
+
+    # set optical properties
+    relative_change = 1 + contrast
+
+    g = 0.9  # anisotropy factor
+    mua0 = 0.02  # background absorption [1/mm]
+    mus0 = 0.67 / (1 - g)  # background scattering [1/mm]
+    mua1 = mua0 * relative_change  # absorption of perturbation [1/mm]
+    refr_index = 1.4  # refractive index
+
+    medium_bg = medium.copy()
+
+    # turn off the black formatter for this section
+    # fmt: off
+    medium.optical_properties = np.array(
+        [[0, 0, 1, 1],
+         [mua0, mus0, g, refr_index],
+         [mua1, mus0, g, refr_index]])
+    
+    medium_bg.optical_properties = np.array([
+        [0, 0, 1, 1],
+        [mua0, mus0, g, refr_index],
+        [mua0, mus0, g, refr_index]]) # background only
+    # fmt: on
+
+    return medium_bg, medium
+
+
+def two_balls_2d_sensors(noptodes: int, medium: Medium):
+    """
+    Builds a 2D sensor geometry for the two balls medium.
+
+    Parameters
+    ----------
+    noptodes : int
+        The number of optodes.
+    medium : Medium
+        The 2D medium with two balls of different optical properties.
+
+    Returns
+    -------
+    sensors : SensorGeometry
+        The 2D sensor geometry for the two balls medium.
+    """
+    det_pos = np.zeros((noptodes, 2))
+    src_pos = np.zeros((noptodes, 2))
+
+    # Create an array of i values
+    i_values = np.arange(noptodes)
+
+    # Calculate phi for q and m
+    phi_sensors = i_values / noptodes * np.pi / 2 + np.pi / 4
+    phi_sources = (i_values + 0.5) / noptodes * np.pi / 2 + np.pi / 4
+
+    # Calculate sensor and source positions
+    scaling_factor = 1
+
+    center_point = np.array([medium.ny // 2, medium.nx // 2])
+    det_pos = (
+        scaling_factor
+        * r_head_mm
+        * np.vstack((np.sin(phi_sensors), np.cos(phi_sensors))).T
+        + center_point
+    )
+    src_pos = (
+        r_head_mm * np.vstack((np.sin(phi_sources), np.cos(phi_sources))).T
+        + center_point
+    )
+    src_dirs = center_point - src_pos
+    src_dirs = src_dirs / np.linalg.norm(src_dirs, axis=1)[:, None]  # normalize
+
+    # add a column of zeros to cast as 3D
+    src_pos = np.hstack((medium.nz // 2 * np.ones((noptodes, 1)), src_pos))
+    src_dirs = np.hstack((np.zeros((noptodes, 1)), src_dirs))
+    det_pos = np.hstack((medium.nz // 2 * np.ones((noptodes, 1)), det_pos))
+    det_pos = np.hstack((det_pos, np.ones((noptodes, 1))))
+
+    sensors = SensorGeometry(src_pos, det_pos, src_dirs)
+    return sensors
---
+++ 3d/solver.py
@@ -0,0 +1,182 @@
+from medium import Medium
+from sensor_geometry import SensorGeometry
+import pmcx
+import numpy as np
+import jax.numpy as jnp
+from jax import jit
+
+
+class Solver:
+    def __init__(
+        self, medium: Medium, sensors: SensorGeometry, tstart=0, tend=1e-8, tstep=5e-10
+    ):
+        self.medium = medium
+        self.sensors = sensors
+        self.tstart = tstart
+        self.tend = tend
+        self.tstep = tstep
+        self.nt = int((tend - tstart) / tstep)
+
+    def forward(self, src_idx, random_seed=1):
+        """
+        Implements the forward monte carlo solver.
+        """
+
+        config = {
+            "seed": random_seed,
+            "nphoton": 1e7,
+            "vol": self.medium.volume,
+            "tstart": self.tstart,
+            "tend": self.tend,
+            "tstep": self.tstep,
+            "srcpos": self.sensors.src_pos[src_idx],
+            "srcdir": self.sensors.src_dirs[src_idx],
+            "prop": self.medium.optical_properties,
+            "detpos": self.sensors.det_pos,
+            "replaydet": -1,
+            "issavedet": 1,
+            "issrcfrom0": 1,
+            "issaveseed": 1,
+            # 'unitinmm': 1.8,
+            # 'maxdetphoton': 1e10,
+        }
+
+        result = pmcx.mcxlab(config)
+        return result, config
+
+    def get_td_data(self, res: dict, optical_properties: np.ndarray = None):
+        """
+        Get time domain data from pmcx.mcxlab() output.
+
+        Parameters
+        ----------
+        res : dict
+            output of pmcx.run()
+        optical_properties : np.ndarray, optional
+            The optical properties of each medium. If None, use the optical properties
+            from the medium object.
+
+        Returns
+        -------
+        data : (ntimebins, ndetectors) np.ndarray
+        """
+
+        if optical_properties is None:
+            optical_properties = self.medium.optical_properties
+
+        detp = res["detp"]
+        weights = detweight(detp, optical_properties)
+        tof = dettime(detp, optical_properties, self.medium.grid_resolution_mm)
+
+        data = np.zeros((self.nt, self.sensors.ndet))
+
+        for i in range(self.sensors.ndet):
+            hist, bin_edges = np.histogram(
+                tof[detp["detid"] == i + 1],
+                bins=self.nt,
+                weights=weights[detp["detid"] == i + 1],
+                range=(self.tstart, self.tend),
+            )
+            t = bin_edges[:-1]
+            data[:, i] = hist
+
+        return data
+
+
+@jit
+def invert(dphi, J):
+    # J has shape (nz, ny, nx, nt, ndetectors)
+    # mua_bg has shape (nz, ny, nx)
+    # dphi has shape (ndetectors, nt)
+
+    # we want for dmua s.t. J @ dmua = dphi
+
+    nz, ny, nx, nt, ndetectors, nsources = J.shape
+
+    # Reshape J to 2D matrix for matrix operation
+    J_reshaped = J.reshape((nz * ny * nx, nt * ndetectors * nsources)).T
+
+    # Flatten dphi to a 1D vector
+    dphi_flattened = dphi.flatten()
+
+    # Use JAX for the least-squares solution
+    dmua, residuals, rank, s = jnp.linalg.lstsq(J_reshaped, dphi_flattened, rcond=None)
+
+    # Reshape dmua back to the original dimensions
+    dmua_reshaped = dmua.reshape((nz, ny, nx))
+
+    return dmua_reshaped
+
+
+def jacobian(forward_result, cfg):
+    # one must define cfg['seed'] using the returned seeds
+    cfg["seed"] = forward_result["seeds"]
+
+    # one must define cfg['detphotons'] using the returned detp data
+    cfg["detphotons"] = forward_result["detp"]["data"]
+
+    # tell mcx to output absorption (μ_a) Jacobian
+    cfg["outputtype"] = "jacobian"
+
+    result = pmcx.run(cfg)
+
+    J = result["flux"]  # Jacobian of shape (nz, ny, nx, nt, ndetectors)
+
+    # Flip sign of jacobian (since dphi = -J @ dmua)
+    J = -J
+    return J
+
+
+def dettime(detp, prop, unitinmm=1):
+    """
+    Recalculate the detected photon time using partial path data and
+    optical properties (for perturbation Monte Carlo or detector readings).
+
+    Parameters:
+    detp (dict): The second output from mcxlab. detp must be a dictionary.
+    prop (list): Optical property list, as defined in the cfg.prop field of mcxlab's input.
+    unitinmm (float): Voxel edge-length in mm. If ignored, assume to be 1 (mm).
+
+    Returns:
+    dett (numpy.ndarray): Recalculated detected photon time based on the partial path data and optical property table.
+    """
+
+    R_C0 = 3.335640951981520e-12  # inverse of light speed in vacuum
+
+    # Check the number of media
+    medianum = len(prop)
+
+    dett = np.zeros(detp["ppath"].shape[0])
+    for i in range(medianum - 1):
+        refractive_index = prop[i + 1][3]  # refractive index
+        dett += refractive_index * detp["ppath"][:, i] * R_C0 * unitinmm
+    return dett
+
+
+def detweight(detp, prop):
+    """
+    Calculates the detector weights for each measurement.
+
+    Parameters
+    ----------
+    detp : numpy.ndarray
+        The detector path information from pmcx.run().
+    prop : numpy.ndarray
+        The optical properties of each medium.
+
+    Returns
+    -------
+    numpy.ndarray
+        The detector weights for each measurement.
+    """
+    # For each measurement, we multiply the path length in each medium by the absorption coefficient, and exponentiate
+    # to get the intensity.
+
+    pathlengths = detp["ppath"]
+    absorption_coefficients = np.array(prop)[1:, 0]  # exclude background
+
+    weights = np.exp(
+        -pathlengths @ absorption_coefficients
+    )  # * cfg['unitinmm']) # (nmeas,)
+
+    return weights
