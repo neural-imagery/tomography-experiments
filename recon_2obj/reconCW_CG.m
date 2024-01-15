64e022be5e191a160fed361cf54aaadcb9a37ec8
Raffi Hotter <raphael.hotter@gmail.com>
2024-01-14 17:28:02 -0800

    abstractions

--- recon_2obj/reconCW_CG.m
+++ recon_2obj/reconCW_CG.m
@@ -3,93 +3,23 @@
 
 function reconCW_CG(depth, separation, square_width, change, nopt)
 
-%% 0. Define some parameters
-
-% mesh parameters
-rad   = 70; % mesh radius [mm]
-nsect = 6;  % number of sectors
-nring = 32; % number of rings
-nbnd  = 2;  % number of boundary rings
-
-% optical parameters @ 850nm
-refind = 1.4;                % refractive index
-c0     = 0.3;                % speed of light in vacuum [mm/ps]
-cm     = c0/refind;          % speed of light in the medium [mm/ps]
-mua0   = 0.02;               % background absorption [1/mm]
-mus0   = 0.67;               % background scattering [1/mm];
-
-bx = 64; by = 64; % solution basis: grid dimension
-
 % optimization parameters
 tau    = 1e-2; % regularisation parameter
 beta   = 0.01; % TV regularisation parameter
 tolCG  = 1e-8; % CG convergence criterion
 itrmax = 100;   % CG max. iterations
 
-%% 1. Define the target mesh
-[vtx,idx,eltp] = mkcircle(rad,nsect,nring,nbnd);
-hMesh = toastMesh (vtx,idx,eltp);
-n = hMesh.NodeCount;
-hMesh.Display
+% Get the medium
+[mua, mua1, mus, mus1, kap, ref, qvec, mvec, hMesh, cm] = twoSquaresMedium();
 
-%% 2. Define the source and sensor geometry
-distrib = 'quarter';
-for i=1:nopt
-  switch distrib
-      case 'full'
-          phiq = (i-1)/nopt*2*pi; 
-          phim = (i-0.5)/nopt*2*pi;
-      case 'quarter'
-          phiq = (i-1)/nopt*pi/2+pi/4;
-          phim = (i-0.5)/nopt*pi/2+pi/4;
-  end
-  source(i,:) = rad*[cos(phiq), sin(phiq)];
-  sensor(i,:) = rad*[cos(phim), sin(phim)];
-end
-
-hMesh.SetQM(source,sensor);
-qvec = real(hMesh.Qvec('Neumann','Gaussian',2));
-mvec = real(hMesh.Mvec('Gaussian',2,refind));
-
-hold on
-plot(source(:,1),source(:,2),'ro','MarkerFaceColor','r');
-plot(sensor(:,1),sensor(:,2),'bsquare','MarkerFaceColor','b');
-hold off
-legend('','Source', 'Sensor');
-
-%% 3. Assign optical properties
-
-% Set up the mapper between FEM and inverse solution bases
-hBasis = toastBasis (hMesh, [bx by]);
-dx = 2*rad/bx; dy = 2*rad/by;
-
-% Set up homogeneous initial parameter estimates
-mua = ones(n,1) * mua0; mus = ones(n,1) * mus0; ref = ones(n,1) * refind;
-kap = 1./(3*(mua+mus));
-
-swidth    = round(square_width/dx);
-s1x_start = round(bx/2 - (separation/2)/dx - swidth/2);
-s2x_start = round(bx/2 + (separation/2)/dx - swidth/2);
-sy_start  = round(by-depth/dy-swidth/2);
-
-muaim = zeros(bx,by); musim = muaim;
-muaim(s1x_start:s1x_start+swidth, sy_start:sy_start+swidth) = change*mua0;
-muaim(s2x_start:s2x_start+swidth, sy_start:sy_start+swidth) = change*mua0;
-musim(s1x_start:s1x_start+swidth, sy_start:sy_start+swidth) = change*mus0;
-musim(s2x_start:s2x_start+swidth, sy_start:sy_start+swidth) = change*mus0;
-
-mua1 = mua + hBasis.Map('B->M',muaim);
-mus1 = mus + hBasis.Map('B->M',musim);
-
-figure(2); clf;
-subplot(2,2,1); hMesh.Display(mua1); axis off; title('\mu_a target');
-subplot(2,2,2); hMesh.Display(mus1); axis off; title('\mu_s target');
+%% ---------
 
 %% 4. Generate target data
 data = log(mvec' * (dotSysmat(hMesh, mua1, mus1, ref, 0)\qvec));
 data = reshape(data, [], 1);
+% m = length(data); % number of measurements
 
-%% 5. Setup inverse solver
+%% Setup inverse solver
 
 % reset parameters to background values
 proj = log(mvec' * (dotSysmat(hMesh, mua, mus, ref, 0)\qvec));
@@ -155,9 +85,9 @@ while (itr <= itrmax) && (err > tolCG*err0) && (errp-err > tolCG)
     step = toastLineSearch(x, d, step, err, @objective);
     x = x + d*step;
     
-    smua = x(1:size(x)/2)/cm; mua = hBasis.Map ('S->M', smua);
-    skap = x(size(x)/2+1:size(x))/cm; 
-    smus = 1./(3*skap) - smua; mus = hBasis.Map ('S->M', smus);
+    % smua = x(1:size(x)/2)/cm; mua = hBasis.Map ('S->M', smua);
+    % skap = x(size(x)/2+1:size(x))/cm; 
+    % smus = 1./(3*skap) - smua; mus = hBasis.Map ('S->M', smus);
     
     figure(2);
     subplot(2,2,3); hMesh.Display(mua); axis off; title('\mu_a recon');
